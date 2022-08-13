fe9820c23f1c516c2316ef826b4c353953a0d593
Nathan David Hill <nathanhill123@gmail.com>
2022-08-13 18:25:43 +0200

    0. Text does not go beyond the MudPaper [STILL IN PROGRESS]

--- 00_ToDoApp/DataBase/ToDos.cs
+++ 00_ToDoApp/DataBase/ToDos.cs
@@ -3,7 +3,9 @@
 public class ToDos
 {
     public int Id { get; set; }
+    [MaxLength (50)]
     public string? Title { get; set; }
+    [MaxLength (50)]
     public string? Comment { get; set; }
     public DateTime Created { get; set; }
     public bool IsDone { get; set; }
